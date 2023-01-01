bd1b13b4e16bc3fbfb65c982c664986ac31e545e
achrafash <aitsidihammou.achraf@gmail.com>
2023-01-01 16:36:23 +0100

    feat: add markdown pages with default layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,17 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+function defaultLayoutPlugin() {
+    return function (tree, file) {
+        file.data.astro.frontmatter.layout = "@/layouts/note-layout.astro";
+    };
+}
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    markdown: {
+        remarkPlugins: [defaultLayoutPlugin],
+        extendDefaultPlugins: true,
+    },
+    integrations: [tailwind({ config: { applyBaseStyles: false } })],
 });
